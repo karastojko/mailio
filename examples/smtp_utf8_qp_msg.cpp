d1fd0e0ca04e2d816c009f7922e2863e532cdfef
karas <contact@alepho.com>
2021-06-17 22:50:59 +0200

    Fixed UTF-8 literal for MSVC in the SMTP example.

--- examples/smtp_utf8_qp_msg.cpp
+++ examples/smtp_utf8_qp_msg.cpp
@@ -43,25 +43,25 @@ int main()
         msg.add_cc_recipient(mail_address("mailio library", "mailio@yahoo.com"));// add CC recipient
         msg.add_bcc_recipient(mail_address("mailio library", "mailio@zoho.com"));
 
-        msg.subject(u8"smtp utf8 quoted printable Ð¿Ð¾ÑÑÐºÐ°");
+        msg.subject(u8"smtp utf8 quoted printable message");
         // create message in Cyrillic alphabet
         // set Transfer Encoding to Quoted Printable and set Content Type to UTF8
         msg.content_transfer_encoding(mime::content_transfer_encoding_t::QUOTED_PRINTABLE);
         msg.content_type(message::media_type_t::TEXT, "plain", "utf-8");
         msg.content(u8"Ово је јако дугачка порука која има и празних линија и предугачких линија. Није јасно како ће се текст преломити\r\n"
-            "па се надам да ће то овај текст показати.\r\n"
-            "\r\n"
-            "Треба видети како познати мејл клијенти ломе текст, па на\r\n"
-            "основу тога дорадити форматирање мејла. А можда и нема потребе, јер libmailio није замишљен да се\r\n"
-            "бави форматирањем текста.\r\n"
-            "\r\n\r\n"
-            "У сваком случају, после провере латинице треба урадити и проверу utf8 карактера одн. ћирилице\r\n"
-            "и видети како се прелама текст када су карактери вишебајтни. Требало би да је небитно да ли је енкодинг\r\n"
-            "base64 или quoted printable, јер се ascii карактери преламају у нове линије. Овај тест би требало да\r\n"
-            "покаже има ли багова у логици форматирања,\r\n"
-            "а исто то треба проверити са парсирањем.\r\n"
-            "\r\n\r\n\r\n\r\n"
-            "Овде је и провера за низ празних линија.");
+            u8"па се надам да ће то овај текст показати.\r\n"
+            u8"\r\n"
+            u8"Треба видети како познати мејл клијенти ломе текст, па на\r\n"
+            u8"основу тога дорадити форматирање мејла. А можда и нема потребе, јер libmailio није замишљен да се\r\n"
+            u8"бави форматирањем текста.\r\n"
+            u8"\r\n\r\n"
+            u8"У сваком случају, после провере латинице треба урадити и проверу utf8 карактера одн. ћирилице\r\n"
+            u8"и видети како се прелама текст када су карактери вишебајтни. Требало би да је небитно да ли је енкодинг\r\n"
+            u8"base64 или quoted printable, јер се ascii карактери преламају у нове линије. Овај тест би требало да\r\n"
+            u8"покаже има ли багова у логици форматирања,\r\n"
+            u8"а исто то треба проверити са парсирањем.\r\n"
+            u8"\r\n\r\n\r\n\r\n"
+            u8"Овде је и провера за низ празних линија.");
 
         // use a server with plain (non-SSL) connectivity
         smtp conn("smtp.mailserver.com", 587);
