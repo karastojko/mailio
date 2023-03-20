613041565e7fbecfe3deb84fa3737551b06a7841
karas <contact@alepho.com>
2023-03-20 21:17:24 +0100

    Test for formatting UTF8 subject containing an emoji character.

--- test/test_message.cpp
+++ test/test_message.cpp
@@ -1924,6 +1924,36 @@ BOOST_AUTO_TEST_CASE(format_qq_subject_dash)
 
 
 /**
+Formatting a message with UTF-8 subject containing an emoji character.
+
+@pre  None.
+@post None.
+**/
+BOOST_AUTO_TEST_CASE(format_qq_subject_emoji)
+{
+    message msg;
+    msg.header_codec(message::header_codec_t::QUOTED_PRINTABLE);
+    msg.from(mail_address("mailio", "adresa@mailio.dev"));
+    msg.add_recipient(mail_address("mailio", "adresa@mailio.dev"));
+    ptime t = time_from_string("2016-02-11 22:56:22");
+    time_zone_ptr tz(new posix_time_zone("+00:00"));
+    local_date_time ldt(t, tz);
+    msg.date_time(ldt);
+    msg.subject(u8"\U0001F381\u017Divi godinu dana na ra\u010Dun Super Kartice");
+    msg.content("test");
+
+    string msg_str;
+    msg.format(msg_str);
+    BOOST_CHECK(msg_str == "From: mailio <adresa@mailio.dev>\r\n"
+        "To: mailio <adresa@mailio.dev>\r\n"
+        "Date: Thu, 11 Feb 2016 22:56:22 +0000\r\n"
+        "Subject: =?UTF-8?Q?=F0=9F=8E=81=C5=BDivi_godinu_dana_na_ra=C4=8Dun_Super_Kartice?=\r\n"
+        "\r\n"
+        "test\r\n");
+}
+
+
+/**
 Parsing simple message.
 
 @pre  None.
